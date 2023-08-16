58fc2835ae627ead6220e3ebb467ac802284550e
polarSaunderson <Dominic.saunderson@monash.edu>
2023-08-16 13:36:48 +1000

    initial commit of `cat2` and `calc_magnitude`; `count_decimal_places` can handle vectors

---
+++ R/cat2.R
@@ -0,0 +1,27 @@
+cat2 <- function(x, show = TRUE) {
+  #' Concatenate the name and variable
+  #'
+  #' @description This function is likely useful because I don't know how to
+  #'   debug "correctly". When developing functions, it can be hard to know
+  #'   where the code is going wrong. Using `cat2()` prints out a variable and
+  #'   it's name. Sprinkle these at key locations in functions to see what is
+  #'   happening where.
+  #'
+  #' @param x The variable that should be displayed. It's name and value will be
+  #'   shown. Lists are unlisted. Not tested with complex data types - use with
+  #'   caution!
+  #' @param show logical If FALSE, the function does nothing; it is useful if
+  #'   there are multiple `cat2()` calls throughout; set a variable at the
+  #'   beginning and use that as the 'show' argument in each `cat2()` call.
+  #'   Simply changing that to FALSE allows the function to be run without all
+  #'   the calls, but the calls can remain in place if further `cat2()` use is
+  #'   likely.
+  #'
+  #' @export
+
+  # Code -----------------------------------------------------------------------
+  if (isTRUE(show)) {
+    naming <- deparse(substitute(x))
+    cat("\n", naming, ":", unlist(x), "\n")
+  }
+}
--- R/count_decimal_places.R
+++ R/count_decimal_places.R
@@ -5,17 +5,104 @@ count_decimal_places <- function(x) {
   #'   to round, but aren't always sure what precision we need to round to. The
   #'   code was taken directly from this stackoverflow answer by daroczig:
   #'   [https://stackoverflow.com/questions/5173692/how-to-return-number-of-decimal-places-in-r](),
-  #'   and I do not really understand how it works.
+  #'   and I do not entirely understand how it works.
   #'
   #' @param x numeric: The value
   #'
   #' @export
 
   # Code -----------------------------------------------------------------------
-  if (abs(x - round(x)) > .Machine$double.eps^0.5) {
-    nchar(strsplit(sub('0+$', '', as.character(x)),
-                   ".", fixed = TRUE)[[1]][[2]])
-  } else {
-    return(0)
+  # Preallocate
+  xDP <- integer(length(x))
+  machPrec <- .Machine$double.eps^0.5
+
+  #
+  for (ii in x) {
+    iiIndex <- which(x == ii)
+    if (abs(ii - round(ii)) > machPrec) {
+      xDP[iiIndex] <- nchar(strsplit(sub('0+$', '', as.character(ii)),
+                                     ".", fixed = TRUE)[[1]][[2]])
+    } else {
+      xDP[iiIndex] <- 0
+    }
   }
+  return(xDP)
 }
+  #
+  # if (abs(x - round(x)) > .Machine$double.eps^0.5) {
+  #   nchar(strsplit(sub('0+$', '', as.character(x)),
+  #                  ".", fixed = TRUE)[[1]][[2]])
+  # } else {
+  #   return(0)
+  # }
+# }
+
+calc_magnitude <- function(x) {
+  #' Return the magnitude of a number
+  #'
+  #' @description Magnitudes are considered powers of 10, so 1:9.999... are all
+  #'   magnitude 0, 10-99.999... are magnitude 1, 0.1:0.999... are -1, etc. The
+  #'   sign of the number makes no difference.
+  #'
+  #' @param x The number.
+  #'
+  #' @export
+
+  # Code -----------------------------------------------------------------------
+  showCat <- FALSE # only here for testing purposes
+
+  xMag <- integer(length(x))
+  for (ii in x) {
+    iiIndex <- which(x == ii)
+    iiString <- format(abs(ii), scientific = FALSE)
+      cat2(iiString, showCat)
+    iiSplit  <- strsplit(iiString, split = ".", fixed = TRUE)
+      cat2(iiSplit, showCat)
+
+    # Approach depends on number magnitude
+    if (abs(ii) < 1) {
+      iiDecimals  <- iiSplit[[1]][[2]]
+        cat2(iiDecimals, showCat)
+      iiDeciSplit <- strsplit(iiDecimals, "") |> unlist() |> as.numeric()
+        cat2(iiDeciSplit, showCat)
+      iiNonZeroes <- which(iiDeciSplit != 0)
+        cat2(iiNonZeroes, showCat)
+      firstNonZero <- iiNonZeroes[1]
+        cat2(firstNonZero, showCat)
+      xMag[iiIndex] <- -firstNonZero
+    } else {
+      iiIntegers <- iiSplit[[1]][[1]]
+        cat2(iiIntegers, showCat)
+      iiIntSplit <- strsplit(iiIntegers, "")
+        cat2(iiIntSplit, showCat)
+      iiLength <- length(iiIntSplit[[1]])
+        cat2(iiLength, showCat)
+      xMag[iiIndex] <- iiLength - 1
+    }
+  }
+  return(xMag)
+}
+
+#' calc_magnitude <- function(x) {
+#'   #' Return the magnitude of a number
+#'   #'
+#'   #' @description Simply counts how many numbers are to the left of the decimal
+#'   #'   point. Returns zero if the absolute value is less than 1.
+#'   #'
+#'   #' @param x The number
+#'   #'
+#'   #' @export
+#'
+#'   # Code -----------------------------------------------------------------------
+#'   xMag <- integer(length(x))
+#'   for (ii in x) {
+#'     iiIndex <- which(x == ii)
+#'     if (abs(ii) < 1) {
+#'       xMag[iiIndex] <- 0
+#'     } else {
+#'       xMag[iiIndex] <- strsplit(as.character(round(ii)), "") |>
+#'         unlist() |> length()
+#'     }
+#'   }
+#'   return(xMag)
+#' }
