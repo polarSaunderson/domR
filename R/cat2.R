6e8124d7b6a0352986fcd0ee05839347a0278bbd
polarSaunderson <Dominic.saunderson@monash.edu>
2023-08-24 21:16:13 +1000

    initial commit of cat3

--- R/cat2.R
+++ R/cat2.R
@@ -9,7 +9,8 @@ cat2 <- function(x, show = TRUE) {
   #'
   #' @param x The variable that should be displayed. It's name and value will be
   #'   shown. Lists are unlisted. Not tested with complex data types - use with
-  #'   caution!
+  #'   caution! If you just want the variable (and not the name), stick with
+  #'   `cat()` or use `cat3)` which basically adds "\n" before and after x.
   #' @param show logical If FALSE, the function does nothing; it is useful if
   #'   there are multiple `cat2()` calls throughout; set a variable at the
   #'   beginning and use that as the 'show' argument in each `cat2()` call.
@@ -23,5 +24,32 @@ cat2 <- function(x, show = TRUE) {
   if (isTRUE(show)) {
     naming <- deparse(substitute(x))
     cat("\n", naming, ":", unlist(x), "\n")
+  }
+}
+
+cat3 <- function(..., show = TRUE) {
+  #' Concatenate a variable
+  #'
+  #' @description This function is simply `cat()` but adds a newline "\n" before
+  #'   and after to quicken things up.
+  #'
+  #' @param ... The variable/s that should be displayed. The values will be
+  #'   collapsed down with a space between them and then shown using `cat()`.
+  #'   Not tested with complex data types - use with caution! If you want the
+  #'   name of the variable and the variable, use `cat2()`.
+  #' @param show logical If FALSE, the function does nothing; it is useful if
+  #'   there are multiple `cat3()` calls throughout; set a variable at the
+  #'   beginning and use that as the 'show' argument in each `cat3()` call.
+  #'   Simply changing that to FALSE allows the function to be run without all
+  #'   the calls, but the calls can remain in place if further `cat3()` use is
+  #'   likely.
+  #'
+  #' @export
+
+  # Code -----------------------------------------------------------------------
+  if (isTRUE(show)) {
+    dots <- list(...)
+    dots <- paste(dots, collapse = " ")
+    cat("\n", dots, "\n")
   }
 }
