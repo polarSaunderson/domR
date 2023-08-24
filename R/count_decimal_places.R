4a8de15c1fcad169e60c0527240d1cb51385b571
polarSaunderson <Dominic.saunderson@monash.edu>
2023-08-24 17:45:15 +1000

    corrected `count_decimal_places` to handle if no decimal places

--- R/count_decimal_places.R
+++ R/count_decimal_places.R
@@ -19,19 +19,23 @@ count_decimal_places <- function(x) {
   # Loop through values
   for (ii in x) {
     iiIndex <- which(x == ii)
-    if (abs(ii - round(ii)) > machPrec) { # diff is enough for computer to see
-      # convert to text to count
-      iiSplit <-  strsplit(sub('0+$', '',
-                               format(ii, digits = 15,
-                                      scientific = FALSE)), # not Xe+Y
-                           ".", fixed = TRUE)[[1]]
-      if (length(iiSplit) == 1) {       # if nothing after decimal
+    if (!is.na(ii)) {
+      if (abs(ii - round(ii)) > machPrec) { # diff is enough for computer to see
+        # convert to text to count
+        iiSplit <-  strsplit(sub('0+$', '',
+                                 format(ii, digits = 15,
+                                        scientific = FALSE)), # not Xe+Y
+                             ".", fixed = TRUE)[[1]]
+        if (length(iiSplit) == 1) {       # if nothing after decimal
+          xDP[iiIndex] <- 0
+        } else {                          # get part after decimal place
+          xDP[iiIndex] <- nchar(iiSplit[[2]])
+        }
+      } else {
         xDP[iiIndex] <- 0
-      } else {                          # get part after decimal place
-        xDP[iiIndex] <- nchar(iiSplit[[2]])
       }
     } else {
-      xDP[iiIndex] <- 0
+      xDP[iiIndex] <- NaN
     }
   }
   return(xDP)
