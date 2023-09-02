f220c1e2ca9ab81649b1c9b7d5b61e34ea87707b
polarSaunderson <Dominic.saunderson@monash.edu>
2023-09-02 19:43:09 +1000

    added 'nStart' and 'nEnd' arguments to `print_line()`

--- R/print_line.R
+++ R/print_line.R
@@ -1,5 +1,5 @@
 ##
-print_line <- function(lineType = "=", width = 22){
+print_line <- function(lineType = "=", nStart = 1, nEnd = 1, width = 22){
   #' Print a line to the console to help delineate output
   #'
   #' @description Useful when working interactively in RStudio, and can be
@@ -7,17 +7,20 @@ print_line <- function(lineType = "=", width = 22){
   #'
   #' @param lineType "string": The symbol/s that will be printed to create a
   #'   line across the console.
-  #' @param width numeric : How many times should the above be repeated? Usually
+  #' @param nStart numeric: How many `\n` should be included *before* the line?
+  #' @param nEnd numeric: How many `\n` should be included *after* the line?
+  #' @param width numeric: How many times should the above be repeated? Usually
   #'   unnecessary, but change it if screen is particularly wide or narrow.
   #'
   #' @examples
   #'   print_line()
-  #'   print_line("-", 10)
-  #'   print_line("+-", 25)
+  #'   print_line("=", 1, 0)
+  #'   print_line("-", width = 10)
+  #'   print_line("+-", width = 25)
   #'
   #' @export
 
   # Code ----------------------------------------------------------------------!
   lineString <- paste0(rep(lineType, width * 3), collapse = "")
-  cat("\n", lineString, "\n", sep = "")
+  cat(rep("\n", nStart), lineString, rep("\n", nEnd), sep = "")
 }
