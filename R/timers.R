357df149e2b77083b6b04d904f66b4c10a405a2c
polarSaunderson <Dominic.saunderson@monash.edu>
2023-09-04 18:35:36 +1000

    `end_timer` only uses `beepr` if available & w/o error if not

--- R/timers.R
+++ R/timers.R
@@ -1,10 +1,10 @@
 start_timer <- function() {
   #' Starts the timer
   #'
-  #' @description This function and `end_timer()` are mainly because I can never
-  #'   remember the syntax/name of `proc.time()`. Simply call `start_timer()`
-  #'   before the process you are timing begins, and then call `end_timer()`
-  #'   afterwards, feeding it with the output of `start_timer()`.
+  #' @description This function and [end_timer()] are mainly because I can never
+  #'   remember the syntax/name of [proc.time()]. Simply call [start_timer()]
+  #'   before the process you are timing begins, and then call [end_timer()]
+  #'   afterwards, feeding it with the output of [start_timer()].
   #'
   #' @examples
   #' \dontrun{
@@ -21,15 +21,15 @@ start_timer <- function() {
 end_timer <- function(startTime, name = "", sound = TRUE){
   #' Ends the timer
   #'
-  #' @description This function and `start_timer()` are mainly because I can
-  #'   never remember the syntax/name of `proc.time()`. Simply call
-  #'   `start_timer()` before the process you are timing begins, and then call
-  #'   `end_timer()` afterwards, feeding it with the output of `start_timer()`.
+  #' @description This function and [start_timer()] are mainly because I can
+  #'   never remember the syntax/name of [proc.time()]. Simply call
+  #'   [start_timer()] before the process you are timing begins, and then call
+  #'   [end_timer()] afterwards, feeding it with the output of [start_timer()].
   #'   This function invisibly returns the time of it finishing, so it can be
-  #'   used in a subsequent `end_timer()` function call and the timing of
+  #'   used in a subsequent [end_timer()] function call and the timing of
   #'   processes can be chained together. See the usage examples.
   #'
-  #' @param startTime Use the output from `start_timer()` as input here
+  #' @param startTime Use the output from [start_timer()] as input here
   #' @param name "string": Name of the process that has been timed
   #' @param sound BINARY: Should a sound be played upon completion? Requires
   #'    `beepr`.
@@ -62,7 +62,9 @@ end_timer <- function(startTime, name = "", sound = TRUE){
   print(timeTaken)
   print_line()
   if (isTRUE(sound)) {
-    beepr::beep()
+    if (system.file(package = "beepr") != "") {
+      beepr::beep()
+    }
   }
 
   return(invisible(proc.time()))
