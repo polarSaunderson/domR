40c60b30303db2982b0913fe1267cdf383d387f8
polarSaunderson <Dominic.saunderson@monash.edu>
2023-09-17 14:11:00 +1000

    separated `catx` functions individual files; `cat2` and `cat4` invisibly return 'x'

--- R/cat2.R
+++ R/cat2.R
@@ -1,25 +1,35 @@
 cat2 <- function(x, show = TRUE, name = NULL) {
-  #' Concatenate the name and variable
+  #' Print out a variable's name and value
   #'
-  #' @description This function is likely useful because I don't know how to
-  #'   debug "correctly". When developing functions, it can be hard to know
-  #'   where the code is going wrong. Using `cat2()` prints out a variable and
-  #'   it's name. Sprinkle these at key locations in functions to see what is
-  #'   happening where. If it is a list, try [cat_list()].
+  #' @description Using [cat2()] prints out a variable and it's name.
+  #'
+  #'   This function is useful because I don't know how to debug "correctly".
+  #'   For example, when developing functions, it can be hard to know where the
+  #'   code is going wrong, particularly if there are no true errors.  However,
+  #'   the returned value can still be incorrect, but it is not clear at which
+  #'   stage a variable has which value.
+  #'
+  #'   Sprinkling the [cat2()] function at key locations in functions allows us
+  #'   to see what is happening where within a function.
   #'
   #' @param x The variable that should be displayed. It's name and value will be
-  #'   shown. Lists are unlisted. Not tested with complex data types - use with
-  #'   caution! If you just want the variable (and not the name), stick with
-  #'   `cat()`, or use `cat3()`, which basically adds a newline `\n` before and after x.
-  #' @param show LOGICAL: If FALSE, the function does nothing; it is useful if
-  #'   there are multiple `cat2()` calls throughout; set a variable at the
-  #'   beginning and use that as the 'show' argument in each `cat2()` call.
-  #'   Simply changing that to FALSE allows the function to be run without all
-  #'   the calls, but the calls can remain in place if further `cat2()` use is
-  #'   likely.
-  #' @param name "string": Should an alternative name be used? By default,
-  #'   the name of x is used. However, if [cat2()] is nested, this will likely
-  #'   just end up as "x" for every variable.
+  #'   shown. Lists are fed to [cat_list()].
+  #'
+  #'   If you just want the variable (and not the name), stick with `cat()`, or
+  #'   use `cat3()`, which basically adds a newline `\n` before and after x.
+  #'
+  #'   **Note:** This function has not been tested with complex data types - use
+  #'   with caution!
+  #'
+  #' @param show LOGICAL: If FALSE, the function just returns the input but does
+  #'   not print anything out. This behaviour is helpful as it allows multiple
+  #'   [cat2()] calls to be added throughout a function, and a single variable
+  #'   at the start can be used to turn the printing on for debugging, or off
+  #'   for use. Useful when it is likely that further debugging will be needed.
+  #' @param name "string": Should an alternative name be used? By default, the
+  #'   name of 'x' is used. However, if [cat2()] is nested, this will likely
+  #'   just end up as "x" for every variable, so the true name can be entered
+  #'   here.
   #'
   #' @export
 
@@ -42,59 +52,5 @@ cat2 <- function(x, show = TRUE, name = NULL) {
       cat(paste0("\n", name, " :"), x, "\n")
     }
   }
-}
-
-cat3 <- function(..., show = TRUE) {
-  #' Concatenate a variable
-  #'
-  #' @description This function is simply `cat()` but adds a newline `\n` before
-  #'   and after to quicken things up. It is not exactly the same output format,
-  #'   and still in progress.
-  #'
-  #' @param ... The variable/s that should be displayed. The values will be
-  #'   collapsed down with a space between them and then shown using `cat()`.
-  #'   Not tested with complex data types - use with caution! If you want the
-  #'   name of the variable and the variable, use `cat2()`; if location,
-  #'   `cat4()`.
-  #' @param show logical If FALSE, the function does nothing; it is useful if
-  #'   there are multiple `cat3()` calls throughout; set a variable at the
-  #'   beginning and use that as the 'show' argument in each `cat3()` call.
-  #'   Simply changing that to FALSE allows the function to be run without all
-  #'   the calls, but the calls can remain in place if further `cat3()` use is
-  #'   likely.
-  #'
-  #' @export
-
-  # Code -----------------------------------------------------------------------
-  if (isTRUE(show)) {
-    dots <- list(...)
-    dots <- paste(dots, collapse = " ")
-    cat("\n", dots, "\n")
-  }
-}
-
-
-cat4 <- function(x, show = TRUE, skipCatCall = TRUE) {
-  #' Concatenate the name and variable, and show the location
-  #'
-  #' @description This function combines [which_line()] and [cat2()]. It
-  #'   therefore shows the value and name of a variable, and the location of it.
-  #'
-  #' @param x The variable to show.
-  #' @inheritParams cat2
-  #' @inheritParams which_line
-  #'
-  #' @export
-
-  # Code -----------------------------------------------------------------------
-  if (isTRUE(show)) {
-    print_line("-", 1, 0)
-    xName <- deparse(substitute(x))
-    if (is.list(x)) {
-      cat_list(x, show = show, name = xName)
-    } else {
-      cat2(x, show = show, name = xName)
-    }
-    which_line(skipCatCall = TRUE)
-  }
+  return(invisible(x))
 }
---
+++ R/cat3.R
@@ -0,0 +1,30 @@
+cat3 <- function(..., show = TRUE) {
+  #' Print out input easier
+  #'
+  #' @description This function is simply [cat()] but adds a newline `\n` before
+  #'   and after to quicken things up.
+  #'
+  #'   **Note:** The output format is not always exactly the same as [cat()]. It
+  #'   is still in progress.
+  #'
+  #' @param ... The variable/s that should be displayed. The values will be
+  #'   collapsed down with a space between them and then shown using [cat()].
+  #'   Not tested with complex data types - use with caution! If you want the
+  #'   name of the variable and the variable, use [cat2()]; if the location too,
+  #'   use [cat4()].
+  #' @param show logical If FALSE, the function does nothing; it is useful if
+  #'   there are multiple [cat3()] calls throughout; set a variable at the
+  #'   beginning and use that as the 'show' argument in each [cat3()] call.
+  #'   Simply changing that to FALSE allows the function to be run without all
+  #'   the calls, but the calls can remain in place if further [cat3()] use is
+  #'   likely.
+  #'
+  #' @export
+
+  # Code -----------------------------------------------------------------------
+  if (isTRUE(show)) {
+    dots <- list(...)
+    dots <- paste(dots, collapse = " ")
+    cat("\n", dots, "\n")
+  }
+}
