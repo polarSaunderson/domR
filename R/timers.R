f6ba75c70c15233e764397d430f80e397c45d8b7
polarSaunderson <dominic.saunderson@monash.edu>
2023-10-14 20:02:23 +1100

    `start_timer` prints the start time, and accepts a process name

--- R/timers.R
+++ R/timers.R
@@ -1,11 +1,16 @@
-start_timer <- function() {
+start_timer <- function(...) {
   #' Starts the timer
   #'
   #' @description This function and [end_timer()] are mainly because I can never
   #'   remember the syntax/name of [proc.time()]. Simply call [start_timer()]
   #'   before the process you are timing begins, and then call [end_timer()]
-  #'   afterwards, feeding it with the output of [start_timer()].
+  #'   afterwards, feeding it with the output of [start_timer()]. See examples.
   #'
+  #'   These functions will be developed in future versions to allow multiple
+  #'   levels of timing.
+  #'
+  #' @param ... "string" Name of the process that is being timed; all arguments
+  #'   are displayed in the order entered using [cat()].
   #' @examples
   #' \dontrun{
   #'   start <- start_timer()
@@ -15,6 +20,8 @@ start_timer <- function() {
   #' @export
 
   # Code -----------------------------------------------------------------------
+  cat3(">>>", ...,                                   # print process name
+       "started at:", now("t"), "...", nStart = 0)   # and current time
   return(proc.time())
 }
 
@@ -31,7 +38,7 @@ end_timer <- function(startTime, ..., sound = TRUE){
   #'
   #' @param startTime Use the output from [start_timer()] as input here
   #' @param ... "string": Name of the process that has been timed; all arguments
-  #'   will be displayed in the order entered using [cat3()].
+  #'   will be displayed in the order entered using [cat()].
   #' @param sound BINARY: Should a sound be played upon completion? Requires
   #'    `beepr`.
   #'
@@ -59,9 +66,9 @@ end_timer <- function(startTime, ..., sound = TRUE){
 
   # Code -----------------------------------------------------------------------
   timeTaken <- proc.time() - startTime
-  cat3(..., "done! It took: ")
+  cat3(">>>", ..., "done! It took:", nStart = 0)
   print(timeTaken)
-  print_line()
+  print_line(nStart = 0)
   if (isTRUE(sound)) {
     if (system.file(package = "beepr") != "") {
       beepr::beep()
